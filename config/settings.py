a856499d41d831360f105905630ce3b723f99b2e
ousmanelagrange <ousmanemfochive4@gmail.com>
2024-02-02 23:01:32 +0100

    allowed host fixed

--- config/settings.py
+++ config/settings.py
@@ -152,6 +152,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+ALLOWED_HOSTS = ['*']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -159,4 +162,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-ALLOWED_HOSTS = []
